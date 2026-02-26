85ee7abbf274c7bd59fc543eab1be093a62b2f8b
unknown <zaky.shadra.tif25@polban.ac.id>
2026-02-26 10:36:55 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach(el => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+});
